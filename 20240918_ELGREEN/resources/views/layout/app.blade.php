ca9e5e7726ab5286579649eb428b9c115c170ea7
bpbdkotakediri <bpbd.kotakediri113@gmail.com>
2024-09-28 07:31:15 +0700

    pembuatan footer

---
+++ 20240918_ELGREEN/resources/views/layout/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'HOME') | ELGREEN</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('image/elgreen.png') }}" />
+    <link rel="stylesheet" href="{{ asset('bootstrap-5.3.3-dist\css\bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom-khoi.css') }}">
+    @stack('csss')
+    
+</head>
+
+<body>
+    @include('layout.fe-navbar')
+    @yield('content')
+
+
+    @include('layout.fe-footer')
+
+    <script src="{{ asset('bootstrap-5.3.3-dist\js\bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/custom-khoi.js') }}"></script>
+    @stack('jss')
+</body>
+
+</html>
---
+++ 20240918_ELGREEN/resources/views/layout/fe-footer.blade.php
@@ -0,0 +1,25 @@
+<div class="row" style="background-color: rgba(31,31,31,1)">
+    <div class="col-3" style="background-color:rgb(149, 219, 196)">
+
+    </div>
+    <div class="col-6 " style="background-color:rgb(31, 31, 31,1)">
+        <div class="row">
+            <div class="col-4">
+                <span style="" style="color:rgba(135,234,121,1) !important;font-size:30px;line:height:normal">EL GREENx</span>
+                <nav class="nav flex-column">
+                    <a class="nav-link" style="color:rgba(153,153,151,1) !important" href="{{route('home')}}">Home</a>
+                    <a class="nav-link" style="color:rgba(153,153,151,1) !important" href="{{route('shop')}}">Shop</a>
+                    <a class="nav-link" style="color:rgba(153,153,151,1) !important" href="{{route('about')}}">About</a>
+                </nav>
+            </div>
+            <div class="col-4">
+            </div>
+            <div class="col-4">
+
+            </div>
+        </div>
+    </div>
+    <div class="col-3" style="background-color:rgb(206, 255, 127)">
+
+    </div>
+</div>
